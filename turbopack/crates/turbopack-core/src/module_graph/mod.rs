b455aea37bd9ed6a02078ab897a806514f6a61cd
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-24 10:43:27 +0100

    GraphEntries: Option<ChunkGroupType>

--- turbopack/crates/turbopack-core/src/module_graph/mod.rs
+++ turbopack/crates/turbopack-core/src/module_graph/mod.rs
@@ -139,7 +139,7 @@ impl VisitedModules {
     }
 }
 
-pub type GraphEntriesT = Vec<(Vec<ResolvedVc<Box<dyn Module>>>, ChunkGroupType)>;
+pub type GraphEntriesT = Vec<(Vec<ResolvedVc<Box<dyn Module>>>, Option<ChunkGroupType>)>;
 
 #[turbo_tasks::value(transparent)]
 pub struct GraphEntries(GraphEntriesT);
@@ -704,7 +704,10 @@ impl ModuleGraph {
     }
 
     #[turbo_tasks::function]
-    pub fn from_module(module: ResolvedVc<Box<dyn Module>>, ty: ChunkGroupType) -> Vc<Self> {
+    pub fn from_module(
+        module: ResolvedVc<Box<dyn Module>>,
+        ty: Option<ChunkGroupType>,
+    ) -> Vc<Self> {
         Self::from_single_graph(SingleModuleGraph::new_with_entries(Vc::cell(vec![(
             vec![module],
             ty,
