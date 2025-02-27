7e9da0ef54e46bfed5c90b10786459134f2633de
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-27 20:17:59 +0900

    Ignore declare: true

--- turbopack/crates/turbopack-ecmascript/src/analyzer/graph.rs
+++ turbopack/crates/turbopack-ecmascript/src/analyzer/graph.rs
@@ -1504,6 +1504,11 @@ impl VisitAstPath for Analyzer<'_> {
         decl: &'ast FnDecl,
         ast_path: &mut AstNodePath<AstParentNodeRef<'r>>,
     ) {
+        // It's not a real declaration
+        if decl.declare {
+            return;
+        }
+
         let old = replace(
             &mut self.cur_fn_return_values,
             Some(get_fn_init_return_vals(decl.function.body.as_ref())),
@@ -1609,6 +1614,11 @@ impl VisitAstPath for Analyzer<'_> {
         decl: &'ast ClassDecl,
         ast_path: &mut AstNodePath<AstParentNodeRef<'r>>,
     ) {
+        // It's not a real declaration
+        if decl.declare {
+            return;
+        }
+
         self.add_value_from_expr(
             decl.ident.to_id(),
             // TODO avoid clone
@@ -1625,6 +1635,11 @@ impl VisitAstPath for Analyzer<'_> {
         n: &'ast VarDecl,
         ast_path: &mut AstNodePath<AstParentNodeRef<'r>>,
     ) {
+        // It's not a real declaration
+        if n.declare {
+            return;
+        }
+
         let old = self.var_decl_kind;
         self.var_decl_kind = Some(n.kind);
         n.visit_children_with_ast_path(self, ast_path);
